9918e865b55fc3879126d688682fd798281d4c0a
Jonne Huotari <jonne@huotari.me>
2017-11-25 23:13:29 +0200

    asd

--- app/constants/Spotify.js
+++ app/constants/Spotify.js
@@ -10,7 +10,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 
 const SPOTIFY_CLIENT_ID = (inDev ? '27981c0745974f79b2a0a7fa029cbd58' : 'feab0673a206476483fa11a3d81f260a');
 
-let redirectUri = (inDev ? 'http://localhost:3000/callback' : 'http://localhost:3000/callback');
+let redirectUri = (inDev ? 'http://localhost:3000/callback' : 'http://spoti.fyi/callback');
 
 // credentials are optional
 let spotifyCredentials = {
